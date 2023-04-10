7179f367b9904a051592d734e355cd00551cafe5
Denys Zimmermann <denysz@gmx.ch>
2023-04-10 10:30:54 +0200

    feat: rooms implemented and buttons assigned

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "sideled.h"
 
 
-void event_handler_checkbox(struct _lv_obj_t * obj, lv_event_t event);
 void event_handler_button(struct _lv_obj_t * obj, lv_event_t event);
 void init_gui_elements();
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
@@ -15,6 +14,7 @@ lv_obj_t * led;
 
 lv_obj_t * red_checkbox;
 lv_obj_t * blue_checkbox;
+lv_obj_t * green_checkbox;
 
 lv_obj_t * off_checkbox;
 lv_obj_t * on_checkbox;
@@ -22,50 +22,16 @@ lv_obj_t * blink_checkbox;
 
 lv_obj_t * left_button; //not used anymore
 lv_obj_t * right_button; //not used anymore
-lv_obj_t * wardrope_button;
+lv_obj_t * wardrobe_button;
 lv_obj_t * kitchen_button;
 lv_obj_t * living_button;
 lv_obj_t * bedroom_button;
 lv_obj_t * haal_button;
 
-void event_handler_checkbox(struct _lv_obj_t * obj, lv_event_t event) {
-  if(event == LV_EVENT_VALUE_CHANGED ) {
-    if(
-      (obj == red_checkbox || obj == blue_checkbox) &&
-      (lv_checkbox_is_checked(red_checkbox) || lv_checkbox_is_checked(blue_checkbox))
-      ) {
-      lv_checkbox_set_checked(blue_checkbox, obj == blue_checkbox ? lv_checkbox_is_checked(blue_checkbox) : false);
-      lv_checkbox_set_checked(red_checkbox, obj == red_checkbox ? lv_checkbox_is_checked(red_checkbox) : false);
-    }
-    if(
-      (obj == off_checkbox || obj == on_checkbox || obj == blink_checkbox) &&
-      (lv_checkbox_is_checked(off_checkbox) || lv_checkbox_is_checked(on_checkbox) || lv_checkbox_is_checked(blink_checkbox))
-      ) {
-      lv_checkbox_set_checked(off_checkbox, obj == off_checkbox ? lv_checkbox_is_checked(off_checkbox) : false);
-      lv_checkbox_set_checked(on_checkbox, obj == on_checkbox ? lv_checkbox_is_checked(on_checkbox) : false);
-      lv_checkbox_set_checked(blink_checkbox, obj == blink_checkbox ? lv_checkbox_is_checked(blink_checkbox) : false);
-    }
-    bool is_ready = (
-        ((
-        (lv_checkbox_is_checked(red_checkbox) && !lv_checkbox_is_checked(blue_checkbox)) ||
-        (!lv_checkbox_is_checked(red_checkbox) && lv_checkbox_is_checked(blue_checkbox))
-      ) &&
-      (
-        (!lv_checkbox_is_checked(off_checkbox) && lv_checkbox_is_checked(on_checkbox) && !lv_checkbox_is_checked(blink_checkbox)) ||
-        (!lv_checkbox_is_checked(off_checkbox) && !lv_checkbox_is_checked(on_checkbox) && lv_checkbox_is_checked(blink_checkbox))
-      )) || (
-        (lv_checkbox_is_checked(off_checkbox) && !lv_checkbox_is_checked(on_checkbox) && !lv_checkbox_is_checked(blink_checkbox))
-      ));
-    lv_obj_set_click(left_button, is_ready);
-    lv_obj_set_click(right_button, is_ready);
-    lv_obj_set_state(left_button, is_ready ? LV_STATE_DEFAULT : LV_STATE_DISABLED);
-    lv_obj_set_state(right_button, is_ready ? LV_STATE_DEFAULT : LV_STATE_DISABLED);
-  } 
-}
 
 void event_handler_button(struct _lv_obj_t * obj, lv_event_t event) {
   if(event == LV_EVENT_PRESSED) {
-    uint8_t led_wardrope = (obj == wardrope_button ? 0 : 6);
+    uint8_t led_wardrobe = (obj == wardrobe_button ? 0 : 6);
     uint8_t led_kitchen = (obj == kitchen_button ? 6 : 12);
     uint8_t led_livingroom = (obj == living_button ? 12 : 19);
     uint8_t led_bedroom = (obj == bedroom_button ? 19 : 24);
@@ -74,27 +40,26 @@ void event_handler_button(struct _lv_obj_t * obj, lv_event_t event) {
     uint8_t state = SIDELED_STATE_OFF;
     if(lv_checkbox_is_checked(on_checkbox)) state = SIDELED_STATE_ON;
     if(lv_checkbox_is_checked(blink_checkbox)) state = SIDELED_STATE_BLINK;
-    set_sideled_color(led_wardrope,led_kitchen, color);
-    set_sideled_state(led_wardrope,led_kitchen, state);
+    set_sideled_color(led_wardrobe,led_kitchen, color);
+    set_sideled_state(led_wardrobe,led_kitchen, state);
   }
 }
 
 void init_gui_elements() {
   add_label("1. Select Color", 10, 10);
-  red_checkbox = add_checkbox("Red", 10, 40, event_handler_checkbox);
-  blue_checkbox = add_checkbox("Blue", 120, 40, event_handler_checkbox);
+  red_checkbox = add_checkbox("Red", 10, 40);
+  blue_checkbox = add_checkbox("Blue", 120, 40);
+  green_checkbox = add_checkbox("Green", 230, 40);
   add_label("2. Select Mode", 10, 70);
-  off_checkbox = add_checkbox("Off", 10, 100, event_handler_checkbox);
-  on_checkbox = add_checkbox("On", 120, 100, event_handler_checkbox);
-  blink_checkbox = add_checkbox("Blink", 200, 100, event_handler_checkbox);
-  add_label("3. Apply Color to side:", 10, 140);
-  left_button = add_button("Apply Left", event_handler_button, 0, 170, 150, 50);
-  right_button = add_button("Apply Right", event_handler_button, 160, 170, 150, 50);
-
-  lv_obj_set_click(left_button, false);
-  lv_obj_set_click(right_button, false);
-  lv_obj_set_state(left_button, LV_STATE_DISABLED);
-  lv_obj_set_state(right_button, LV_STATE_DISABLED);
+  off_checkbox = add_checkbox("Off", 10, 100);
+  on_checkbox = add_checkbox("On", 120, 100);
+  blink_checkbox = add_checkbox("Blink", 200, 100);
+  add_label("3. Select room:", 10, 140);
+  living_button = add_button("Livingroom", event_handler_button, 0, 170, 75, 30);
+  kitchen_button = add_button("Kitchen", event_handler_button, 85, 170, 75, 30);
+  bedroom_button = add_button("Bedroom", event_handler_button, 170, 170, 75, 30);
+  haal_button = add_button("Hallway", event_handler_button, 0, 210, 75, 30);
+  wardrobe_button = add_button("Wardrobe", event_handler_button, 85, 210, 75, 30);
 }
 
 
