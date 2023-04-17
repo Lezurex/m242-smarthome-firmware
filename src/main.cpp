502d39a9328dfd67e0ce3371638e19ac1954c065
Lenny Angst <lenny@familie-angst.ch>
2023-04-17 02:15:42 +0200

    feat(MQTT): :sparkles: Setup MQTT connection and callback
    It kind of works, however, it always reconnects for some reason, even though the connections seems to be fine (status 0).

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,52 @@
 #include "networking.h"
 #include "sideled.h"
 
-
-void event_handler_button(struct _lv_obj_t * obj, lv_event_t event);
+void event_handler_button(struct _lv_obj_t *obj, lv_event_t event);
 void init_gui_elements();
-void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void mqtt_callback(char *topic, byte *payload, unsigned int length);
 
 unsigned long next_lv_task = 0;
 
-lv_obj_t * led;
+lv_obj_t *led;
 
-lv_obj_t * red_checkbox;
-lv_obj_t * blue_checkbox;
-lv_obj_t * green_checkbox;
+lv_obj_t *red_checkbox;
+lv_obj_t *blue_checkbox;
+lv_obj_t *green_checkbox;
 
-lv_obj_t * off_checkbox;
-lv_obj_t * on_checkbox;
-lv_obj_t * blink_checkbox;
+lv_obj_t *off_checkbox;
+lv_obj_t *on_checkbox;
+lv_obj_t *blink_checkbox;
 
-lv_obj_t * left_button; //not used anymore
-lv_obj_t * right_button; //not used anymore
-lv_obj_t * wardrobe_button;
-lv_obj_t * kitchen_button;
-lv_obj_t * living_button;
-lv_obj_t * bedroom_button;
-lv_obj_t * haal_button;
+lv_obj_t *left_button;  // not used anymore
+lv_obj_t *right_button; // not used anymore
+lv_obj_t *wardrobe_button;
+lv_obj_t *kitchen_button;
+lv_obj_t *living_button;
+lv_obj_t *bedroom_button;
+lv_obj_t *hall_button;
 
-
-void event_handler_button(struct _lv_obj_t * obj, lv_event_t event) {
-  if(event == LV_EVENT_PRESSED) {
+void event_handler_button(struct _lv_obj_t *obj, lv_event_t event)
+{
+  if (event == LV_EVENT_PRESSED)
+  {
     uint8_t led_wardrobe = (obj == wardrobe_button ? 0 : 6);
     uint8_t led_kitchen = (obj == kitchen_button ? 6 : 12);
     uint8_t led_livingroom = (obj == living_button ? 12 : 19);
     uint8_t led_bedroom = (obj == bedroom_button ? 19 : 24);
-    uint8_t led_haal = (obj == haal_button ? 24 : 30);
+    uint8_t led_hall = (obj == hall_button ? 24 : 30);
     CRGB color = lv_checkbox_is_checked(blue_checkbox) ? CRGB::Blue : CRGB::Red;
     uint8_t state = SIDELED_STATE_OFF;
-    if(lv_checkbox_is_checked(on_checkbox)) state = SIDELED_STATE_ON;
-    if(lv_checkbox_is_checked(blink_checkbox)) state = SIDELED_STATE_BLINK;
-    set_sideled_color(led_wardrobe,led_kitchen, color);
-    set_sideled_state(led_wardrobe,led_kitchen, state);
+    if (lv_checkbox_is_checked(on_checkbox))
+      state = SIDELED_STATE_ON;
+    if (lv_checkbox_is_checked(blink_checkbox))
+      state = SIDELED_STATE_BLINK;
+    set_sideled_color(led_wardrobe, led_kitchen, color);
+    set_sideled_state(led_wardrobe, led_kitchen, state);
   }
 }
 
-void init_gui_elements() {
+void init_gui_elements()
+{
   add_label("1. Select Color", 10, 10);
   red_checkbox = add_checkbox("Red", 10, 40);
   blue_checkbox = add_checkbox("Blue", 120, 40);
@@ -58,33 +61,53 @@ void init_gui_elements() {
   living_button = add_button("Livingroom", event_handler_button, 0, 170, 75, 30);
   kitchen_button = add_button("Kitchen", event_handler_button, 85, 170, 75, 30);
   bedroom_button = add_button("Bedroom", event_handler_button, 170, 170, 75, 30);
-  haal_button = add_button("Hallway", event_handler_button, 0, 210, 75, 30);
+  hall_button = add_button("Hallway", event_handler_button, 0, 210, 75, 30);
   wardrobe_button = add_button("Wardrobe", event_handler_button, 85, 210, 75, 30);
 }
-
 
 // ----------------------------------------------------------------------------
 // MQTT callback
 // ----------------------------------------------------------------------------
 
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+void mqtt_callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived" + String(topic));
+  auto topicS = String(topic);
+  String room = topicS.substring(0, topicS.lastIndexOf("/"));
+  room = room.substring(room.lastIndexOf("/") + 1);
+
   // Parse Payload into String
-  char * buf = (char *)malloc((sizeof(char)*(length+1)));
+  auto *buf = (char *)malloc((sizeof(char) * (length + 1)));
   memcpy(buf, payload, length);
   buf[length] = '\0';
-  String payloadS = String(buf);
+  auto payloadS = String(buf);
   payloadS.trim();
 
-  if(String(topic) == "example") {
-    if(payloadS == "on") {
+  if (topicS.endsWith("/mode"))
+  {
+    Serial.println("Mode");
+  }
+  else if (topicS.endsWith("/color"))
+  {
+    show_message_box("Color", "Ok", "No", nullptr);
+  }
+  else if (topicS.endsWith("/brightness"))
+  {
+    show_message_box("Brightness", "Ok", "No", nullptr);
+  }
+
+  if (String(topic) == "example")
+  {
+    if (payloadS == "on")
+    {
       lv_led_on(led);
     }
-    if(payloadS == "off") {
+    if (payloadS == "off")
+    {
       lv_led_off(led);
     }
   }
 }
-
 
 // ----------------------------------------------------------------------------
 // UI event handlers
@@ -92,42 +115,49 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 
 String buffer = "";
 
-void event_handler_num(struct _lv_obj_t * obj, lv_event_t event) {
-
+void event_handler_num(struct _lv_obj_t *obj, lv_event_t event)
+{
 }
 
-lv_obj_t * mbox;
-
+lv_obj_t *mbox;
 
 // ----------------------------------------------------------------------------
 // MAIN LOOP
 // ----------------------------------------------------------------------------
 
-void loop() {
-  if(next_lv_task < millis()) {
+void loop()
+{
+  if (next_lv_task < millis())
+  {
     lv_task_handler();
     next_lv_task = millis() + 5;
   }
 
-  // Uncomment the following lines to enable MQTT
-  // mqtt_loop();
+  mqtt_loop();
 }
 
 // ----------------------------------------------------------------------------
 // MAIN SETUP
 // ----------------------------------------------------------------------------
 
-void setup() {
+void setup()
+{
   init_m5();
   init_display();
   Serial.begin(115200);
-  // Uncomment the following lines to enable WiFi and MQTT
-  //lv_obj_t * wifiConnectingBox = show_message_box_no_buttons("Connecting to WiFi...");
-  //lv_task_handler();
-  //delay(5);
-  //setup_wifi();
-  //mqtt_init(mqtt_callback);
-  //close_message_box(wifiConnectingBox);
+
+  // Setup WiFi
+  lv_obj_t *wifiConnectingBox = show_message_box_no_buttons("Connecting to WiFi...");
+  lv_task_handler();
+  delay(5);
+  setup_wifi();
+
+  Serial.println("WiFi connected");
+
+  // Initialize MQTT
+  mqtt_init(mqtt_callback);
+
+  close_message_box(wifiConnectingBox);
   init_gui_elements();
   init_sideled();
 }
--- src/networking.cpp
+++ src/networking.cpp
@@ -3,42 +3,54 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup_wifi() {
+void setup_wifi()
+{
   Serial.println("Connecting to " + String(ssid));
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, passphrase);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.println(".");
   }
   Serial.println("\nSuccess\n");
 }
 
-void mqtt_loop() {
-    if (!client.connected()) {
-        mqtt_re_connect();
+void mqtt_loop()
+{
+  Serial.println(client.state());
+  if (client.state() != MQTT_CONNECTED)
+  {
+    mqtt_re_connect();
   }
   client.loop();
 }
 
-void mqtt_init(MQTT_CALLBACK_SIGNATURE) {
-    client.setServer(mqtt_server, 1883); 
-    client.setCallback(callback); 
-    mqtt_re_connect();
+void mqtt_init(MQTT_CALLBACK_SIGNATURE)
+{
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  mqtt_re_connect();
 }
 
-void mqtt_publish(const char* topic, const char* payload) {
-    client.publish(topic, payload);
+void mqtt_publish(const char *topic, const char *payload)
+{
+  client.publish(topic, payload);
 }
 
-void mqtt_re_connect() {
-  while (!client.connected()) {
+void mqtt_re_connect()
+{
+  while (client.state() != MQTT_CONNECTED)
+  {
     Serial.println("Attempting MQTT connection...");
-    if (client.connect(clientId)) {
-        // Define subscriptions here
-      client.subscribe("lvgl/#");
-    } else {
+    if (client.connect(clientId))
+    {
+      client.subscribe("smarthome/#", 2);
+      Serial.println("MQTT connected");
+    }
+    else
+    {
       Serial.println("failed, rc=");
       Serial.println(client.state());
       Serial.println("try again in 5 seconds");
